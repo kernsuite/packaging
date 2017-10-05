b73ec49ff29c7e1e572b468ce8400d3c5878f389
Gijs Molenaar <gijs@pythonic.nl>
2017-10-05 16:00:25 +0200

    add build script

---
+++ build_all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -ve
+
+# this script will clone all package repositories and build them
+# it will install required dependencies during the run, so it requires sudo access
+#
+# package dependencies: sudo devscripts git git-buildpackage equivs
+
+BASE=git@github.com:eduvpn-debian/
+
+if [ ! -d "build" ]; then
+    mkdir build;
+fi
+
+pushd build
+
+for i in `cat ../packages`; do
+    if [ ! -f "$i.build" ]; then
+        if [ ! -d "$i" ]; then
+            git clone ${BASE}$i
+        fi
+        sudo apt --fix-broken install
+        pushd $i
+        dh_clean
+        git pull
+        sudo mk-build-deps -i || true
+        sudo apt --fix-broken install
+        rm -f *-build-deps_*_all.deb 
+        dh_clean
+        gbp buildpackage --git-ignore-new -us -uc
+        popd
+        touch $i.build
+    fi
+done
