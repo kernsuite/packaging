3bed4cbb13cc152916778941ed5982062d2f7806
Gijs Molenaar <gijs@pythonic.nl>
2018-08-29 06:00:46 +0200

    update scan rules

--- uscan_all.sh
+++ uscan_all.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -ve
 
-# checks if there is an update available for any eduVPN package.
-# Will only print out information.
-
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+KERN_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${KERN_ROOT}/settings.sh
 
 pushd build
 
+rm -f ${KERN_ROOT}/uscan_log
+
 for i in $KERN_PACKAGES; do
+	if [ ! -d "$i" ]; then
+		git clone ${KERN_BASE}$i
+	fi
+
     pushd $i
-    uscan || true
+    git checkout upstream
+    git checkout master
+    uscan --dehs 1>> ${KERN_ROOT}/uscan_log || true
+    #gbp import-orig --uscan
+    #gbp dch -D bionic --commit
+    #gbp buildpackage --git-tag
     popd
 done
